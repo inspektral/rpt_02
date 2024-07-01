ea633e395f658dbad055515a6e8e553ff8d6dc6c
TitoScutari <titoscutari@gmail.com>
2024-07-01 16:10:17 +0200

    main styling

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ import Hydra from 'hydra-synth';
 
 const hydra = new Hydra({
   canvas: document.getElementById('hydra-canvas'),
-  detectAudio: false
+  detectAudio: false,
+  width: window.innerWidth,
+  height: window.innerHeight
 });
 
-osc().out()
+window.addEventListener('resize', () => {
+  hydra.setResolution(window.innerWidth, window.innerHeight);
+});
+
+
+
+hydra.setResolution(window.innerWidth, window.innerHeight);
+noise(4, 0.3).modulateScale(o0).modulateScrollY(o0).mult(osc(5).kaleid(20)).out(o0)
